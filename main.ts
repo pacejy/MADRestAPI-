42185c4b41b851903bc84d0738d54e9ad7df62e1
Jy <jy23557n@pace.edu>
2025-04-26 18:07:32 -0400

    convert to node

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-import products from "./products.json" with { type: "json" };
-import categories from "./categories.json" with { type: "json" };
+import products from "./products.json";
+import categories from "./categories.json";
 import express from "express";
-import process from "node:process";
-import {z} from "zod";
+import { z } from "zod";
 
 const app = express();
 
@@ -21,7 +20,7 @@ router.get("/products/category/:categoryId", (req, res) => {
 
     res.status(200).json({
         msg: "Success",
-        data: products.filter(product => product.categoryId === categoryId)
+        data: products.filter((product) => product.categoryId === categoryId),
     });
 });
 
@@ -53,9 +52,8 @@ class User {
         public password: string,
         public cart: CartItem[] = [],
         public cartId = 0,
-        public orders: Order[] = [],
-    ) {
-    }
+        public orders: Order[] = []
+    ) {}
 
     static create(email: string, name: string, password: string) {
         const user = new User(User.ALL.length + 1, email, name, password);
@@ -64,31 +62,34 @@ class User {
     }
 
     static byEmail(email: string) {
-        return this.ALL.find(user => user.email === email);
+        return this.ALL.find((user) => user.email === email);
     }
 
     static byId(id: number) {
-        return this.ALL.find(user => user.id === id);
+        return this.ALL.find((user) => user.id === id);
     }
 
     cartTotal() {
-        const subtotal = this.cart.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
-        const shipping = Math.round((subtotal * 0.10) * 100) / 100;
-        const tax = Math.round((subtotal * 0.10) * 100) / 100;
+        const subtotal = this.cart.reduce(
+            (acc, item) => acc + item.product.price * item.quantity,
+            0
+        );
+        const shipping = Math.round(subtotal * 0.1 * 100) / 100;
+        const tax = Math.round(subtotal * 0.1 * 100) / 100;
         const total = Math.round((subtotal + shipping + tax) * 100) / 100;
         const discount = 0;
-        return {subtotal, shipping, tax, total, discount};
+        return { subtotal, shipping, tax, total, discount };
     }
 
     cartJson() {
-        return this.cart.map(({product, quantity, id}) => ({
+        return this.cart.map(({ product, quantity, id }) => ({
             id,
             productId: product.id,
             productName: product.title,
             price: product.price,
             imageUrl: product.image,
             quantity,
-        }))
+        }));
     }
 }
 
@@ -119,54 +120,73 @@ const placeOrderRequest = z.object({
 router.post("/signup", (req, res) => {
     const data = signupRequest.safeParse(req.body);
     if (data.error) {
-        res.status(400).json({ status: 400, msg: "Invalid arguments"});
+        res.status(400).json({ status: 400, msg: "Invalid arguments" });
         return;
     }
 
-    const {email, name, password} = data.data;
+    const { email, name, password } = data.data;
     if (User.byEmail(email)) {
-        res.status(400).json({ status: 400, msg: "Email already registered"});
+        res.status(400).json({ status: 400, msg: "Email already registered" });
         return;
     }
 
     const user = User.create(email, name, password);
     res.status(201).json({
         msg: "Success",
-        data: { id: user.id, email: user.email, name: user.name, username: user.email }
+        data: {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            username: user.email,
+        },
     });
 });
 
 router.post("/login", (req, res) => {
     const data = loginRequest.safeParse(req.body);
     if (data.error) {
-        res.status(400).json({ status: 400, msg: "Invalid arguments"});
+        res.status(400).json({ status: 400, msg: "Invalid arguments" });
         return;
     }
 
-    const {email, password} = data.data;
+    const { email, password } = data.data;
     const user = User.byEmail(email);
     if (!user) {
-        res.status(404).json({ status: 404, msg: "No user with this email exists"});
+        res.status(404).json({
+            status: 404,
+            msg: "No user with this email exists",
+        });
         return;
     } else if (user.password !== password) {
-        res.status(401).json({ status: 401, msg: "Password mismatch"});
+        res.status(401).json({ status: 401, msg: "Password mismatch" });
         return;
     }
 
     res.status(201).json({
         msg: "Success",
-        data: { id: user.id, email: user.email, name: user.name, username: user.email }
+        data: {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            username: user.email,
+        },
     });
 });
 
 router.get("/checkout/:userId/summary", (req, res) => {
     const user = User.byId(+req.params.userId);
     if (!user) {
-        res.status(404).json({ status: 404, msg: "No user with this ID exists"});
+        res.status(404).json({
+            status: 404,
+            msg: "No user with this ID exists",
+        });
         return;
     }
 
-    res.status(200).json({ msg: "Success", data: { items: user.cartJson(), ...user.cartTotal() } });
+    res.status(200).json({
+        msg: "Success",
+        data: { items: user.cartJson(), ...user.cartTotal() },
+    });
 });
 
 const cartRouter = express.Router();
@@ -174,35 +194,45 @@ const cartRouter = express.Router();
 cartRouter.post("/:userId", (req, res) => {
     const data = addtocartRequest.safeParse(req.body);
     if (data.error) {
-        res.status(400).json({ status: 400, msg: "Invalid arguments"});
+        res.status(400).json({ status: 400, msg: "Invalid arguments" });
         return;
     }
 
     const user = User.byId(+req.params.userId);
     if (!user) {
-        res.status(404).json({ status: 404, msg: "No user with this ID exists"});
+        res.status(404).json({
+            status: 404,
+            msg: "No user with this ID exists",
+        });
         return;
     }
 
-    const product = products.find(p => p.id === data.data.productId);
+    const product = products.find((p) => p.id === data.data.productId);
     if (!product) {
-        res.status(404).json({ status: 404, msg: "Product not found"});
+        res.status(404).json({ status: 404, msg: "Product not found" });
         return;
     }
 
-    if (user.cart.find(item => item.product === product)) {
-        res.status(400).json({ status: 400, msg: "Item already exists"});
+    if (user.cart.find((item) => item.product === product)) {
+        res.status(400).json({ status: 400, msg: "Item already exists" });
         return;
     }
 
-    user.cart.push({ product, quantity: data.data.quantity, id: ++user.cartId });
+    user.cart.push({
+        product,
+        quantity: data.data.quantity,
+        id: ++user.cartId,
+    });
     res.status(200).json({ msg: "Success", data: user.cartJson() });
 });
 
 cartRouter.get("/:userId", (req, res) => {
     const user = User.byId(+req.params.userId);
     if (!user) {
-        res.status(404).json({ status: 404, msg: "No user with this ID exists"});
+        res.status(404).json({
+            status: 404,
+            msg: "No user with this ID exists",
+        });
         return;
     }
 
@@ -212,25 +242,28 @@ cartRouter.get("/:userId", (req, res) => {
 cartRouter.put("/:userId/:itemId", (req, res) => {
     const data = addtocartRequest.safeParse(req.body);
     if (data.error) {
-        res.status(400).json({ status: 400, msg: "Invalid arguments"});
+        res.status(400).json({ status: 400, msg: "Invalid arguments" });
         return;
     }
 
     const user = User.byId(+req.params.userId);
     if (!user) {
-        res.status(404).json({ status: 404, msg: "No user with this ID exists"});
+        res.status(404).json({
+            status: 404,
+            msg: "No user with this ID exists",
+        });
         return;
     }
 
-    const product = products.find(p => p.id === data.data.productId);
+    const product = products.find((p) => p.id === data.data.productId);
     if (!product) {
-        res.status(404).json({ status: 404, msg: "Product not found"});
+        res.status(404).json({ status: 404, msg: "Product not found" });
         return;
     }
 
-    const item = user.cart.find(item => item.id === +req.params.itemId);
+    const item = user.cart.find((item) => item.id === +req.params.itemId);
     if (!item) {
-        res.status(404).json({ status: 404, msg: "Item does not exist"});
+        res.status(404).json({ status: 404, msg: "Item does not exist" });
         return;
     }
 
@@ -242,13 +275,18 @@ cartRouter.put("/:userId/:itemId", (req, res) => {
 cartRouter.delete("/:userId/:itemId", (req, res) => {
     const user = User.byId(+req.params.userId);
     if (!user) {
-        res.status(404).json({ status: 404, msg: "No user with this ID exists"});
+        res.status(404).json({
+            status: 404,
+            msg: "No user with this ID exists",
+        });
         return;
     }
 
-    const itemIndex = user.cart.findIndex(item => item.id === +req.params.itemId);
+    const itemIndex = user.cart.findIndex(
+        (item) => item.id === +req.params.itemId
+    );
     if (itemIndex === -1) {
-        res.status(404).json({ status: 404, msg: "Item does not exist"});
+        res.status(404).json({ status: 404, msg: "Item does not exist" });
         return;
     }
 
@@ -261,44 +299,53 @@ const ordersRouter = express.Router();
 ordersRouter.post("/:userId", (req, res) => {
     const data = placeOrderRequest.safeParse(req.body);
     if (data.error) {
-        res.status(400).json({ status: 400, msg: "Invalid arguments"});
+        res.status(400).json({ status: 400, msg: "Invalid arguments" });
         return;
     }
 
     const user = User.byId(+req.params.userId);
     if (!user) {
-        res.status(404).json({ status: 404, msg: "No user with this ID exists"});
+        res.status(404).json({
+            status: 404,
+            msg: "No user with this ID exists",
+        });
         return;
     }
 
     if (!user.cart.length) {
-        res.status(400).json({ status: 400, msg: "Cart is empty"});
+        res.status(400).json({ status: 400, msg: "Cart is empty" });
         return;
     }
 
     const date = new Date();
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
+    const day = String(date.getDate()).padStart(2, "0");
 
-    const {total} = user.cartTotal();
+    const { total } = user.cartTotal();
     user.orders.push({
-      id: user.orders.length + 1,
-      total,
-      date: `${year}-${month}-${day}`,
-      status: "Pending",
-      cart: user.cart
+        id: user.orders.length + 1,
+        total,
+        date: `${year}-${month}-${day}`,
+        status: "Pending",
+        cart: user.cart,
     });
     user.cart = [];
 
-    res.status(200).json({ msg: "Success", data: { id: user.orders.at(-1)!.id } });
+    res.status(200).json({
+        msg: "Success",
+        data: { id: user.orders.at(-1)!.id },
+    });
 });
 
 ordersRouter.get("/:userId", (req, res) => {
     const userId = +req.params.userId;
     const user = User.byId(userId);
     if (!user) {
-        res.status(404).json({ status: 404, msg: "No user with this ID exists"});
+        res.status(404).json({
+            status: 404,
+            msg: "No user with this ID exists",
+        });
         return;
     }
 
@@ -310,7 +357,7 @@ ordersRouter.get("/:userId", (req, res) => {
             status: order.status,
             totalAmount: order.total,
             userId,
-            items: order.cart.map(item => ({
+            items: order.cart.map((item) => ({
                 id: item.id,
                 orderId: order.id,
                 price: item.product.price,
@@ -318,7 +365,7 @@ ordersRouter.get("/:userId", (req, res) => {
                 productName: item.product.title,
                 quantity: item.quantity,
                 userId,
-            }))
+            })),
         })),
     });
 });
